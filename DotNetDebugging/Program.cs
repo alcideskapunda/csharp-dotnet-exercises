30db1241a09cbd35e24a616cf1da8bf9c44cd361
alcideskapunda <alcideskapunda22@gmail.com>
2025-12-15 05:44:01 -0300

    Debug .NET applications interactively using the Visual Studio Code debugger.

---
+++ DotNetDebugging/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+int result = Fibonacci(9);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+    }
+
+    // If n2 is 5 continue, else break.
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
+    return n == 0 ? n1 : n2;
+}
+
+
+// Subistitutos do console.writeline na depuração.
+// Console.WriteLine("This message is readable by the end user.");
+// Trace.WriteLine("This is a trace message when tracing the app.");
+// Debug.WriteLine("This is a debug message just for developers.");
+// Debug.Assert
+
+// Debug.WriteLineIf(count == 0, "The count is 0 and this may cause an exception.");
+
+// bool errorFlag = true;
+// Trace.WriteLineIf(errorFlag, "Error in AppendData procedure.");
+// Debug.WriteIf(errorFlag, "Transaction abandoned.");
+// Trace.Write("Invalid value for data request");
+
+// Debug.WriteLineIf(count == 0, "The count is 0 and this may cause an exception.");
+
+// dotnet run --configuration Release
